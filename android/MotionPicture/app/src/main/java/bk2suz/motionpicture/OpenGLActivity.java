9ee6297c90e1e3b96cea67a040bdcf3e2adc2385
sujoy <sujoykroy@gmail.com>
2017-06-01 00:36:23 +0530

    experimenting with rendering opengl in bitmap

--- android/MotionPicture/app/src/main/java/bk2suz/motionpicture/OpenGLActivity.java
+++ android/MotionPicture/app/src/main/java/bk2suz/motionpicture/OpenGLActivity.java
@@ -1,12 +1,25 @@
 package bk2suz.motionpicture;
 
+import android.graphics.Bitmap;
+import android.opengl.GLES20;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+
+import java.nio.IntBuffer;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.microedition.khronos.opengles.GL10;
 
 import bk2suz.motionpicturelib.Commons.Object3D;
 import bk2suz.motionpicturelib.Commons.PolygonGroup3D;
+import bk2suz.motionpicturelib.Commons.ThreeDGLBitmapRenderer;
+import bk2suz.motionpicturelib.Commons.ThreeDSurfaceRenderer;
 import bk2suz.motionpicturelib.Document;
 import bk2suz.motionpicturelib.Shapes.Shape;
 import bk2suz.motionpicturelib.Shapes.ThreeDShape;
@@ -21,6 +34,8 @@ public class OpenGLActivity extends AppCompatActivity {
     private ThreeDSurfaceView mSurfaceView1;
     private ThreeDSurfaceView mSurfaceView2;
 
+    private ImageView mImageView;
+
     Object3D mObject3D1;
     PolygonGroup3D mPolygonGroup2;
 
@@ -33,6 +48,7 @@ public class OpenGLActivity extends AppCompatActivity {
         mSeekBarObjectRotateY = (SeekBar) findViewById(R.id.seekBarObjectRotateY);
         mSeekBarObjectRotateZ = (SeekBar) findViewById(R.id.seekBarObjectRotateZ);
 
+        mImageView = (ImageView) findViewById(R.id.imageView);
 
         Document mDoc = Document.loadFromResource(getResources(), R.xml.threed2);
         Shape shape = mDoc.getShapeFromPath("myob");
@@ -51,7 +67,7 @@ public class OpenGLActivity extends AppCompatActivity {
         mSurfaceView1 = (ThreeDSurfaceView) findViewById(R.id.surfaceView1);
         //mSurfaceView2 = (ThreeDSurfaceView) findViewById(R.id.surfaceView2);
 
-        mSurfaceView1.setObject3D(mObject3D1);
+        //mSurfaceView1.setObject3D(mObject3D1);
         //mSurfaceView2.setPolygonGroup3D(mPolygonGroup2);
 
         mSeekBarObjectRotateX.setOnSeekBarChangeListener(new ObjectRotationSeekerBarChangeListener("x"));
@@ -60,7 +76,51 @@ public class OpenGLActivity extends AppCompatActivity {
 
         //m3DSurfaceView = new ThreeDSurfaceView(this);
         //setContentView(m3DSurfaceView);
+        //glThread = new ThreeDGLBitmapRenderer.GLThread(this, mObject3D1);
+        //glThread.start();
+
+        handler = new Handler(this.getMainLooper());
+        executor = Executors.newSingleThreadExecutor();
+        Runnable processBitmap = new Runnable() {
+            @Override
+            public void run() {
+                ThreeDGLBitmapRenderer bitmapRenderer= new ThreeDGLBitmapRenderer(100, 100);
+                ThreeDSurfaceRenderer surfaceRenderer = new ThreeDSurfaceRenderer(getApplicationContext());
+                surfaceRenderer.onSurfaceCreated(null, null);
+                surfaceRenderer.onSurfaceChanged(null, 100, 100);
+                surfaceRenderer.setObject3D(mObject3D1);
+                surfaceRenderer.onDrawFrame(null);
+                int mWidth=100;
+                int mHeight=100;
+                IntBuffer ib = IntBuffer.allocate(100*100);
+                IntBuffer ibt = IntBuffer.allocate(100*100);
+                GLES20.glReadPixels(0, 0, mWidth, mHeight, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, ib);
+
+                // Convert upside down mirror-reversed image to right-side up normal image.
+                /*for (int i = 0; i < mHeight; i++) {
+                    for (int j = 0; j < mWidth; j++) {
+                        ibt.put((mHeight-i-1)*mWidth + j, ib.get(i*mWidth + j));
+                    }
+                }*/
+
+                final Bitmap bitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
+                bitmap.copyPixelsFromBuffer(ibt);
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mImageView.setImageBitmap(bitmap);
+                        mImageView.invalidate();
+                        Log.d("GALA", String.format("w=%d", bitmap.getWidth()));
+                    }
+                });
+            }
+        };
+        executor.execute(processBitmap);
     }
+    Handler handler;
+    ExecutorService executor;
+    ThreeDGLBitmapRenderer.GLThread glThread;
 
     class ObjectRotationSeekerBarChangeListener implements SeekBar.OnSeekBarChangeListener {
         String mAxis;
