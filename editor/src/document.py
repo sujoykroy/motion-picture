4d3ed54835b1904a59258b0a0b0ab111cf32a6c6
Sujoy Kumar Roy <sujoykroy@gmail.com>
2020-01-05 13:23:42 +0530

    Revert "small fixes"
    This reverts commit 4df2381b86f868ec452b47d9869589a456f15f63.

--- editor/src/document.py
+++ editor/src/document.py
@@ -768,8 +768,7 @@ class VideoFrameMaker(object):
             exclude_camera_list = []
 
         pre_matrix = self.ctx.get_matrix()
-        multi_shape.draw(self.ctx, drawing_size=self.drawing_size,
-            fixed_border=self.doc_movie.doc.fixed_border,
+        multi_shape.draw(self.ctx, drawing_size=self.drawing_size, fixed_border=True,
             no_camera=False, exclude_camera_list=exclude_camera_list,
             root_shape=multi_shape.parent_shape, pre_matrix=pre_matrix)
 
