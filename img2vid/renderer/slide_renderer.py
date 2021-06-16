e157fccfa47c797db844cbfb6f91660335478ed6
Sujoy Kumar Roy <sujoykroy@gmail.com>
2021-06-16 11:43:21 +0530

    image_slide rendering is split into two parts

--- img2vid/renderer/slide_renderer.py
+++ img2vid/renderer/slide_renderer.py
@@ -41,7 +41,66 @@ class SlideRenderer:
         return RenderInfo(image)
 
     @classmethod
-    def build_image_slide(cls, slide, screen_config, image_config, progress=1):
+    def build_image_slide(cls, slide, screen_config, image_config):
+        render_info = cls.build_image_slide_only_image(slide, screen_config, image_config)
+        file_image= ImageUtils.pil2wand(render_info.image)
+
+        image = cls.build_image_slide_only_captions(
+            slide, screen_config, image_config, render_info.image
+        )
+
+        return RenderInfo(image, render_info.editable_rect, render_info.orig_image_scale)
+
+    @classmethod
+    def build_image_slide_only_captions(cls, slide, screen_config, image_config, canvas):
+        with wand.drawing.Drawing() as context:
+            canvas= ImageUtils.pil2wand(canvas)
+            canvas.units = ImageUtils.PIXEL_PER_INCH
+
+            for caption in slide.active_captions:
+                cap_image = CaptionRenderer.caption2image(
+                    caption=caption, max_width=screen_config.scaled_width,
+                    text_config=image_config, wand_image=True
+                )
+                if not cap_image:
+                    continue
+                cap_pos = Point(0, 0)
+
+                margin = caption.margin
+
+                valign = caption.valign
+                if valign == ImageSlide.CAP_ALIGN_CENTER:
+                    cap_pos.y = (screen_config.scaled_height - cap_image.height - 2 * margin)  *0.5
+                elif valign == ImageSlide.CAP_ALIGN_TOP:
+                    cap_pos.y = margin
+                elif valign == ImageSlide.CAP_ALIGN_BOTTOM:
+                    cap_pos.y = screen_config.scaled_height - cap_image.height - margin
+
+                halign = caption.halign
+                if halign == ImageSlide.CAP_ALIGN_LEFT:
+                    cap_pos.x = margin
+                elif halign == ImageSlide.CAP_ALIGN_RIGHT:
+                    cap_pos.x = screen_config.scaled_width - cap_image.width - margin
+                elif halign == ImageSlide.CAP_ALIGN_CENTER:
+                    cap_pos.x =  (screen_config.scaled_width - cap_image.width -2 * margin)  *0.5
+
+                cap_pos.x = int(cap_pos.x)
+                cap_pos.y = int(cap_pos.y)
+                canvas.composite(image=cap_image, left=cap_pos.x, top=cap_pos.y)
+
+                if caption.focuser:
+                    foucer_image, focuser_pos = CaptionRenderer.create_focuser_image(
+                        caption, cap_image, cap_pos, wand_image=True
+                    )
+                    canvas.composite(image=foucer_image, left=focuser_pos.x, top=focuser_pos.y)
+
+            context(canvas)
+            canvas = ImageUtils.wand2pil(canvas)
+
+        return canvas
+
+    @classmethod
+    def build_image_slide_only_image(cls, slide, screen_config, image_config):
         if slide.TYPE_NAME == VideoSlide.TYPE_NAME:
             file_image = ImageUtils.fetch_video_frame(
                 filepath=slide.filepath,
@@ -58,7 +117,6 @@ class SlideRenderer:
         orig_image_scale = fitted_image.width/file_image.width
         file_image= ImageUtils.pil2wand(fitted_image)
 
-
         with wand.image.Image(resolution=image_config.ppi,
                               width=screen_config.scaled_width,
                               height=screen_config.scaled_height) as canvas:
@@ -72,45 +130,6 @@ class SlideRenderer:
                 img_pos.x = int(img_pos.x)
                 img_pos.y = int(img_pos.y)
                 canvas.composite(image=file_image, left=img_pos.x, top=img_pos.y)
-
-                for caption in slide.active_captions:
-                    cap_image = CaptionRenderer.caption2image(
-                        caption=caption, max_width=screen_config.scaled_width,
-                        text_config=image_config, wand_image=True
-                    )
-                    if not cap_image:
-                        continue
-                    cap_pos = Point(0, 0)
-
-                    margin = caption.margin
-
-                    valign = caption.valign
-                    if valign == ImageSlide.CAP_ALIGN_CENTER:
-                        cap_pos.y = (screen_config.scaled_height - cap_image.height - 2 * margin)  *0.5
-                    elif valign == ImageSlide.CAP_ALIGN_TOP:
-                        cap_pos.y = margin
-                    elif valign == ImageSlide.CAP_ALIGN_BOTTOM:
-                        cap_pos.y = screen_config.scaled_height - cap_image.height - margin
-
-                    halign = caption.halign
-                    if halign == ImageSlide.CAP_ALIGN_LEFT:
-                        cap_pos.x = margin
-                    elif halign == ImageSlide.CAP_ALIGN_RIGHT:
-                        cap_pos.x = screen_config.scaled_width - cap_image.width - margin
-                    elif halign == ImageSlide.CAP_ALIGN_CENTER:
-                        cap_pos.x =  (screen_config.scaled_width - cap_image.width -2 * margin)  *0.5
-
-                    cap_pos.x = int(cap_pos.x)
-                    cap_pos.y = int(cap_pos.y)
-                    canvas.composite(image=cap_image, left=cap_pos.x, top=cap_pos.y)
-
-                    if caption.focuser:
-                        foucer_image, focuser_pos = CaptionRenderer.create_focuser_image(
-                            caption, cap_image, cap_pos, wand_image=True
-                        )
-                        canvas.composite(image=foucer_image, left=focuser_pos.x, top=focuser_pos.y)
-
-                context(canvas)
                 image = ImageUtils.wand2pil(canvas)
 
         editable_rect = Rectangle(
